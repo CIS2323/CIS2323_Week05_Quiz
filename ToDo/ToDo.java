2e11039f3617d6902a88d63797d59b7b16d84300
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-19 18:58:30 -0500

    At least it compiles... but done nothing

--- ToDo/ToDo.java
+++ ToDo/ToDo.java
@@ -1,1 +1,22 @@
 // ToDo Class Definition
+package ToDo;
+
+public class ToDo {
+	public static int count = 0;
+	private String item;
+	private boolean isDone = false;
+	
+	public void addTodo(String i){
+		item = i;
+		count++;
+	}
+	public void markDone(){
+		isDone = true;
+	}
+	public int getCount(){
+		return count;
+	}
+	public String getToDo(){
+		return item;
+	}
+}
--- ToDoList.java
+++ ToDoList.java
@@ -1,1 +1,12 @@
 // ToDo List Main Functions
+import ToDo.*;
+import java.util.Scanner;
+
+public class ToDoList{
+	public static void main(String[] args){
+		
+		final int MAXTODOLIST = 10;
+		
+		ToDo[] itemList = new ToDo[MAXTODOLIST];
+	}
+}
